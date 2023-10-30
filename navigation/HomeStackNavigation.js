23fcca8a5d864535a680d9218095a237fb112ea2
Mayank saini <mayanksaini4455@gmail.com>
2023-10-30 14:58:56 +0530

    Add camera btn

--- navigation/HomeStackNavigation.js
+++ navigation/HomeStackNavigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // import ProductScreen from "../screens/ProductScreen";
 import BottomNavigation from "./BottomNavigation.js"
+import CameraScreen from "../screens/CameraScreen.js";
 
 const HomeStackNavigation = () => {
   const Stack = createNativeStackNavigator();
@@ -12,6 +13,7 @@ const HomeStackNavigation = () => {
       initialRouteName="Main"
     >
       <Stack.Screen name="Main" component={BottomNavigation} />
+      <Stack.Screen name="CameraScreen" component={CameraScreen} />
       {/* <Stack.Screen name="ProductDetail" component={ProductScreen} /> */}
     </Stack.Navigator>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,76 +1,14 @@
 import { View, Text,TouchableOpacity, StyleSheet , Button} from 'react-native'
 import React,{useState} from 'react'
-import { Camera, CameraType , AutoFocus , FlashMode } from 'expo-camera';
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
-
-  const [type, setType] = useState(CameraType.back);
-  const [flash , setFlash] = useState(FlashMode.off)
-  const [permission, requestPermission] = Camera.useCameraPermissions();
-
-  if (!permission) {
-    return <View />;
-  }
-
-  if (!permission.granted) {
-    return (
-      <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
-  }
-
-  function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
-  }
-
-  function toggleFlash() {
-    setFlash(current => (current === FlashMode.off ? FlashMode.on : FlashMode.off));
-  }
-
-
+  const navigate = useNavigation();
   return (
-    <View style={styles.container}>
-    <Camera style={styles.camera} type={type} flashMode={flash}>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-          <Text style={styles.text}>Flip Camera</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={toggleFlash}>
-          <Text style={styles.text}>Camera Flash</Text>
-        </TouchableOpacity>
-      </View>
-    </Camera>
+  <View className="bg-white">
+      <Button onPress={()=>navigate.push("CameraScreen")} title='Camera'></Button>
   </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    border:2,
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-});
 
 export default HomeScreen
