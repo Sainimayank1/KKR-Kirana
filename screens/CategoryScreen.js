9011a4397359dc8c02e167ea2f7203e80f9b4217
Mayank saini <mayanksaini4455@gmail.com>
2023-11-04 10:20:31 +0530

    Done with category section

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,12 +1,236 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Pressable,
+  Image,
+} from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import colors from "../constants/style";
+import { FontAwesome } from "@expo/vector-icons";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const CategoryScreen = () => {
-  return (
-    <View>
-      <Text>CategoryScreen</Text>
-    </View>
-  )
-}
+  const productsCatgory = [
+    {
+      uri: require("../assests/image/CatCarasoul/appliances.webp"),
+      name: "Appliances",
+      index: 1,
+    },
+    {
+      uri: require("../assests/image/CatCarasoul/electronics.webp"),
+      name: "Electronics",
+      index: 2,
+    },
+    {
+      uri: require("../assests/image/CatCarasoul/fashion.webp"),
+      name: "Fashion",
+      index: 3,
+    },
+    {
+      uri: require("../assests/image/CatCarasoul/grocery.webp"),
+      name: "Grocery",
+      index: 4,
+    },
+    {
+      uri: require("../assests/image/CatCarasoul/mobile.webp"),
+      name: "Mobiles",
+      index: 6,
+    },
+    {
+      uri: require("../assests/image/CatCarasoul/toys.webp"),
+      name: "Toys",
+      index: 7,
+    },
+    {
+      uri: require("../assests/image/CatCarasoul/travel.webp"),
+      name: "Travel",
+      index: 8,
+    },
+    {
+      uri: require("../assests/image/CatCarasoul/twoWheeler.webp"),
+      name: "Two Wheeler",
+      index: 9,
+    },
+  ];
 
-export default CategoryScreen
+  const RecentlyViewStore = [
+    {
+      uri: require("../assests/image/Sponsered/BoultEar.webp"),
+      name: "Boult Audio Z40 with Zen ENC Mic, 60H Battery Life, Low Latency Gaming, Made In India, 5.3 Bluetooth Headset",
+      keyFeature: "Brown, In the Ear",
+      category: "Earphone",
+      originalPrice: "5,999",
+      price: "1,499",
+      index: 1,
+    },
+    {
+      uri: require("../assests/image/Sponsered/BoatWatch.webp"),
+      name: "boAt Storm call 1.69 inch HD display with bluetooth calling and 550 nits brightness Smartwatch",
+      keyFeature: "Amoled + Calling",
+      category: "Smart Watch",
+      originalPrice: "10,999",
+      price: "2,999",
+      index: 2,
+    },
+    {
+      uri: require("../assests/image/ItemsBackInStock/mobile.webp"),
+      name: "realme Narzo 30 Pro 5G",
+      keyFeature: "(Blade Silver, 128 GB)  (8 GB RAM)",
+      category: "Mobile",
+      originalPrice: "21,999",
+      price: "20,890",
+      index: 2,
+    },
+    {
+      uri: require("../assests/image/Sponsered/realmeEarphone.webp"),
+      name: "realme Buds Air 3 Neo with up to 30 hours Playback ",
+      keyFeature: " Charge Bluetooth Headset",
+      category: "True Wireless",
+      originalPrice: "3,999",
+      price: "1,199",
+      index: 4,
+    },
+    {
+      uri: require("../assests/image/Sponsered/OppoEarphone.webp"),
+      name: "OPPO Enco Buds 2 with 28 hours Battery life",
+      keyFeature: "Deep Noise Cancellation",
+      category: "Ear Buds",
+      originalPrice: "5,999",
+      price: "1,699",
+      index: 5,
+    },
+    {
+      uri: require("../assests/image/Sponsered/FireBoultWatch.webp"),
+      name: "Fire-Boltt Starlight 2.01'' HD Display Smart Watch Bluetooth Callin",
+      keyFeature: "Stainless Steel Luxury Smartwatch",
+      category: "Smart Watch",
+      originalPrice: "14,999",
+      price: "2,199",
+      index: 6,
+    },
+  ];
+  return (
+    <SafeAreaView className="h-full">
+      <StatusBar backgroundColor={colors.blue} style="light"></StatusBar>
+      {/* Upper conatiner */}
+      <View
+        className=" p-2 justify-between flex flex-row items-center"
+        style={{ backgroundColor: colors.blue }}
+      >
+        {/* Left side */}
+        <Text className="text-white text-lg">All Categories</Text>
+
+        {/* Right side */}
+        <View className="flex flex-row space-x-5">
+          <TouchableOpacity>
+            <FontAwesome name="search" size={18} color="white" />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <FontAwesome name="microphone" size={18} color="white" />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* Down container */}
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        vertical
+        className="flex-1 h-full bg-white"
+      >
+        {/* Product conatiner */}
+        <View className="flex-1 flex flex-row flex-wrap justify-center">
+          {/* Product map */}
+          {productsCatgory.map((item, index) => {
+            return (
+              <Pressable
+                key={index}
+                className="items-center justify-center "
+                style={{ height: wp(28), width: wp(25) }}
+              >
+                <Image
+                  className="h-[60%] w-[70%] rounded-full bg-blue-100 object-contain "
+                  style={{ objectFit: "contain" }}
+                  source={item.uri}
+                ></Image>
+                <Text className=" w-full text-center text-md">
+                  {item?.name?.length >= 10
+                    ? item?.name?.slice(0, 9) + " .."
+                    : item.name}
+                </Text>
+              </Pressable>
+            );
+          })}
+        </View>
+
+        {/* More on Flipkart */}
+        <View className="p-2">
+          <Text className="text-md font-extrabold pb-3">More on Flipkart</Text>
+          <View className="flex-1 flex flex-row flex-wrap justify-center">
+            {/* Product map */}
+            {productsCatgory.map((item, index) => {
+              return (
+                <Pressable
+                  key={index}
+                  className="items-center justify-center "
+                  style={{ height: wp(28), width: wp(25) }}
+                >
+                  <Image
+                    className="h-[60%] w-[70%] rounded-full bg-blue-100 object-contain "
+                    style={{ objectFit: "contain" }}
+                    source={item.uri}
+                  ></Image>
+                  <Text className=" w-full text-center text-md">
+                    {item?.name?.length >= 10
+                      ? item?.name?.slice(0, 9) + " .."
+                      : item.name}
+                  </Text>
+                </Pressable>
+              );
+            })}
+          </View>
+        </View>
+
+        {/* Recently View Store */}
+        <View className="p-2">
+          <Text className="text-md font-[900] pb-2">
+            Recently Viewed Stores
+          </Text>
+          {/* Recently scrollView */}
+          <ScrollView
+            horizontal
+            className="space-x-3"
+            showsHorizontalScrollIndicator={false}
+          >
+            {RecentlyViewStore &&
+              RecentlyViewStore.map((data, index) => {
+                return (
+                  <View
+                    key={index}
+                    style={{ height: wp(35), width: wp(25) }}
+                    className="flex-col items-center border border-gray-300 rounded-md p-2"
+                  >
+                    <Image
+                      source={data.uri}
+                      className="w-full h-[80%]"
+                      style={{ objectFit: "contain" }}
+                    ></Image>
+                    <Text className="text-xs">{data.category}</Text>
+                  </View>
+                );
+              })}
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default CategoryScreen;
